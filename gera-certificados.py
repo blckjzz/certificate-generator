9d07ed09cf2ef27f6f56f31e7428bcc296884172
Diego Cerqueira <xdiegocerqueira@gmail.com>
2017-10-24 01:56:35 -0200

    ajustes na impressao de texto com negrito

--- gera-certificados.py
+++ gera-certificados.py
@@ -5,7 +5,7 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 import time
-
+import datetime
 
 # 186 x 140 = nome do curso
 # 144 x 244 = nome do aluno
@@ -22,46 +22,61 @@ for row in reader:
 
 comeco = time.time()
 
+cargaHoraria = 8
+curso = "Bitcoin para iniciantes"
+curso = curso.decode('utf-8')
 for aluno in alunos:
+    roboto16 = ImageFont.truetype("fonts/Roboto/Roboto-Regular.ttf", 16)
+    roboto12 = ImageFont.truetype("fonts/Roboto/Roboto-Regular.ttf", 12)
+    roboto12Italic = ImageFont.truetype("fonts/Roboto/Roboto-Italic.ttf", 12)
+    roboto12Italic = ImageFont.truetype("fonts/Roboto/Roboto-Italic.ttf", 12)
+    roboto12Bold = ImageFont.truetype("fonts/Roboto/Roboto-Bold.ttf", 12)
+    roboto16Bold = ImageFont.truetype("fonts/Roboto/Roboto-Bold.ttf", 16)
+    roboto16BoldItalic = ImageFont.truetype("fonts/Roboto/Roboto-BoldItalic.ttf", 16)
+    roboto22 = ImageFont.truetype("fonts/Roboto/Roboto-Regular.ttf", 22)
+    roboto22Bold = ImageFont.truetype("fonts/Roboto/Roboto-Bold.ttf", 22)
     print("Gerando o certificado de: " + aluno)
-        
-    ch = 8
-
+       
     img = Image.open("template_certificado.png")
     draw = ImageDraw.Draw(img)
     # font = ImageFont.truetype(<font-file>, <font-size>)
-    font = ImageFont.truetype("fonts/Roboto/Roboto-Regular.ttf", 12)
-    fontBlack = ImageFont.truetype("fonts/Roboto/Roboto-Black.ttf", 16)
     # draw.text((x, y),"Sample Text",(r,g,b))
-    curso = "Bitcoin para iniciantes"
-    curso = curso.decode('utf-8')
 
+    draw.text((150, 135), "Curso" , (0,0,0),font=roboto12)
     #escreve nome do curso
-    draw.text((150, 135), "Curso: " + curso , (0,0,0),font=font)
+    
+    draw.text((185, 135), curso , (0,0,0),font=roboto12Bold)
 
     #Escreve Data
-    data = "24/10/2017 | " + str(ch) + " Horas | ITS Rio"
-    draw.text((150, 150), data , (0,0,0),font=font)
+    data = str(datetime.datetime.today().strftime('%d/%m/%Y')) + " |"
+    data1 = "| ITS Rio"
+    draw.text((150, 150), data , (0,0,0),font=roboto12Italic)
+    draw.text((225, 150), str(cargaHoraria) + " Horas" , (0,0,0),font=roboto12Bold)
+    draw.text((270, 150), data1 , (0,0,0),font=roboto12Italic)
 
 
     #escreve nome do aluno'
     nomeAluno = aluno.decode('utf-8')
-    font = ImageFont.truetype("fonts/Roboto/Roboto-Regular.ttf", 22)
 
-    draw.text((140, 220), nomeAluno.upper() , (0,0,0),font=fontBlack)
+    draw.text((140, 220), nomeAluno.upper() , (0,0,0),font=roboto22Bold)
 
     #escreve o texto sobre que apresenta o nome e etc.
 
-    font = ImageFont.truetype("fonts/Roboto/Roboto-Regular.ttf", 16)
-    #carga horaria do curso
-
-    tx = "Concluiu o curso de extensão " + str(curso)
-
-    tx += "\ncom a carga hora de " + str(ch) + " horas, oferecido pelo Instituto de Tecnologia e Sociedade \ndo Rio."
-    texto = tx.decode('utf-8')
-            
-
-    draw.text((140, 255), texto , (0,0,0),font=font)
+    tx = "Concluiu o curso de extensão "
+    txn = tx.decode('utf-8')
+    txCurso = str(curso).decode('utf-8')
+    txx =  "com a carga hora de " 
+    tx2 = str(cargaHoraria) + " Horas"
+    tx3 = "oferecido pelo Instituto de Tecnologia e Sociedade"
+    tx4 = "do Rio."
+    texto = tx3.decode('utf-8')
+    
+    draw.text((140, 255), txn , (0,0,0),font=roboto16)
+    draw.text((350, 255), txCurso , (0,0,0),font=roboto16BoldItalic)
+    draw.text((140, 275), txx , (0,0,0),font=roboto16)
+    draw.text((290, 275), tx2 , (0,0,0),font=roboto16Bold)
+    draw.text((350, 275), texto , (0,0,0),font=roboto16)
+    draw.text((140, 295), tx4 , (0,0,0),font=roboto16)
 
     #gera o certificado
     img.save("certificados/certificado - " + aluno + ".png")
